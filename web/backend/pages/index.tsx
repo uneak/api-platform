b898b26e56b671c2bebfc27f21c4aab81bad8a31
Marc Galoyer <mgaloyer@uneak.fr>
2022-08-01 18:55:10 -0400

    test

--- web/backend/pages/index.tsx
+++ web/backend/pages/index.tsx
@@ -31,7 +31,7 @@ const Welcome = () => (
         </div>
         <div className="main__content">
           <h1>
-            Welcome to <strong>API Platform</strong>!
+            Page front sur l'admin', on ne devrait pas voir ça, FUCK
           </h1>
           <div className="main__before-starting">
             <p>
